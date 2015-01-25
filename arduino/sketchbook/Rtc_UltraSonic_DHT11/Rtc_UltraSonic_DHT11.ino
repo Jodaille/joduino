80a2b06f7153b44e520a8d0da40df5cff224409d
Jodaille <jody.noury@gmail.com>
2015-01-25 19:23:57 +0100

    add sketch that log datetime with RTC, temperature and humidity with DH11, measure water level with ultrasonic sensor, alert on low water level with two contact only, light sensor

---
+++ arduino/sketchbook/Rtc_UltraSonic_DHT11/Rtc_UltraSonic_DHT11.ino
@@ -0,0 +1,120 @@
+/* Clock
+* cf:
+* https://learn.adafruit.com/ds1307-real-time-clock-breakout-board-kit/understanding-the-code
+*/
+#include <Wire.h>
+#include "RTClib.h" 
+RTC_DS1307 RTC;
+
+// Ultrasonic sensor
+#include <NewPing.h>
+#define TRIGGER_PIN  12
+#define ECHO_PIN     11
+#define MAX_DISTANCE 200 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+// DHT11 sensor
+#include <dht11.h>
+dht11 DHT11;
+#define DHT11PIN A0
+
+/* Low level detection
+cf: http://bateaux.trucs.free.fr/detecteur_eau_sans_composant.html
+*/
+const int lowlevel = A3;
+
+int lightPin = A2;
+
+// Warning led
+const int warning = 13;
+
+// read interval management
+long previousMillis = 0;        // will store last time data was updated
+
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 1000;           // interval at which to blink (milliseconds)
+
+void setup() {
+
+    Serial.begin(9600);
+
+    Wire.begin();
+    RTC.begin();
+
+    if (! RTC.isrunning()) {
+        Serial.println("RTC is NOT running!");
+        // following line sets the RTC to the date & time this sketch was compiled
+        //RTC.adjust(DateTime(__DATE__, __TIME__));
+    }
+
+    // low water level
+    pinMode(lowlevel, INPUT); 
+    
+}
+ 
+void loop() {
+
+    unsigned long currentMillis = millis();
+ 
+    if(currentMillis - previousMillis > interval) {
+        // save the last time we read data 
+        previousMillis = currentMillis;
+
+        int light = analogRead(lightPin);
+
+        digitalWrite(lowlevel, HIGH); // enable internal pull-up
+        if (digitalRead(lowlevel))
+        {
+            //Serial.println("!!! MISSING WATER !!!");
+            digitalWrite(warning, HIGH);
+        }
+        else
+        {
+            digitalWrite(warning, LOW);
+        }
+        digitalWrite(lowlevel, LOW); // disable internal pull-up
+
+        // Sonar distance
+        int uS = sonar.ping();
+        
+        // Time
+        DateTime now = RTC.now();
+        
+        // Temperature and humidity
+        int chk = DHT11.read(DHT11PIN);
+        
+        // Display result
+        Serial.print("{'date':'");
+        Serial.print(now.year(), DEC);
+        Serial.print('-');
+        Serial.print(now.month(), DEC);
+        Serial.print('-');
+        Serial.print(now.day(), DEC);
+        Serial.print('T');
+        Serial.print(now.hour(), DEC);
+        Serial.print(':');
+        Serial.print(now.minute(), DEC);
+        Serial.print(':');
+        Serial.print(now.second(), DEC);
+        Serial.print("',");
+        Serial.print("'waterlevel':");
+        Serial.print(uS / US_ROUNDTRIP_CM);
+        Serial.print(",");
+
+        Serial.print("'light':");
+        Serial.print(light);
+        Serial.print(",");
+
+        Serial.print("'humidity':'");
+        Serial.print((float)DHT11.humidity, 2);
+        Serial.print("',");
+
+        Serial.print("'temperature':");
+        Serial.print((float)DHT11.temperature, 2);
+        Serial.println("}");
+
+        
+        
+    }
+}
