a8042530d4dce8859cd1d5887e346688e6258e72
Jodaille <jody@samssd>
2015-02-23 21:55:07 +0100

    add waterlevel to db

--- ZfJoduino/vendor/jodaille/joduino/docs/create_tables_sample.sql
+++ ZfJoduino/vendor/jodaille/joduino/docs/create_tables_sample.sql
@@ -4,11 +4,12 @@
 --
 
 CREATE TABLE `environment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `sensor_id` int(3) DEFAULT NULL,
   `humidity` int(3) DEFAULT NULL,
   `temperature` varchar(6) DEFAULT NULL,
   `photoresistance` int(4) DEFAULT NULL,
+  `waterlevel` int(11) DEFAULT NULL,
   `msgsent` varchar(60) DEFAULT NULL,
   `msgrcved` varchar(60) DEFAULT NULL,
   `msgdate` datetime DEFAULT NULL,
