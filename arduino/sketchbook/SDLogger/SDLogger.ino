2112ae633dafeb14a8e9311ddcb3ddf962e2e825
Jodaille <jody.noury@gmail.com>
2015-11-07 15:12:04 +0100

    add clock + time to log lines + credits

--- arduino/sketchbook/SDLogger/SDLogger.ino
+++ arduino/sketchbook/SDLogger/SDLogger.ino
@@ -1,3 +1,25 @@
+/**
+* JoBee monitor
+* records temperature and log it on sdCard
+* time from RTclock
+* temperature with DS18B20
+*/
+
+/** 
+* Wire is needed for clock
+*/
+#include <Wire.h>
+
+/*
+* Clock pins:
+* A4 SDA
+* A5 SCL
+* you will find RTClib at :
+* https://github.com/adafruit/RTClib
+*/
+#include "RTClib.h"
+RTC_DS1307 RTC;
+
 /*
   SD card datalogger
 
@@ -18,8 +40,14 @@
 
  This example code is in the public domain.
 
- */
+*/
 #include <OneWire.h>
+
+/**
+* DallasTemperature
+* DS18B20 
+* https://github.com/milesburton/Arduino-Temperature-Control-Library
+*/
 #include <DallasTemperature.h>
 
 #include <SPI.h>
@@ -35,7 +63,14 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 
+/**
+* One measure every five minutes
+*/
 #define FIVEMIN (1000UL * 60 * 5);
+/**
+* every five seconds
+*/
+//#define FIVEMIN (1000UL * 5);
 unsigned long rolltime = millis() + FIVEMIN;
 
 void setup() {
@@ -44,7 +79,10 @@ void setup() {
   /*while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }*/
-
+  //Initialize the serial port, wire library and RTC module
+    Serial.begin(9600);
+    Wire.begin();
+    RTC.begin();
 
   Serial.print("Initializing SD card...");
 
@@ -62,7 +100,20 @@ void setup() {
 void loop() {
   if((long)(millis() - rolltime) >= 0) {
     // make a string for assembling the data to log:
+    DateTime now = RTC.now();
     String dataString = "";
+    dataString += now.day();
+    dataString += "/";
+    dataString += now.month();
+    dataString += "/";
+    dataString += now.year();
+    dataString += " ";
+    dataString += now.hour();
+    dataString += ":";
+    dataString += now.minute();
+    dataString += ":";
+    dataString += now.second();
+    dataString += ",";
     sensors.requestTemperatures();
     // read three sensors and append to the string:
     for (int tempIndex = 0; tempIndex < 3; tempIndex++) {
